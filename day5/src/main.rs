76835ae839ff72d4c9ca1ebec7ab6d1d9d173634
Brian Machado <bmachado@live.ca>
2022-12-20 15:21:15 -0800

    day5  NOT WORKING stack parsing done

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -6,32 +6,39 @@ fn main() {
   const mxsz: usize = strt.len()/3;
   const stcks: u8 = strt.as_bytes()[strt.len()-1]-1;
   const st: usize = stcks as usize;
-  const vect: Vec<i32> = Vec::new();
+  const vect: Vec<u32> = Vec::new();
 
   //store letters as their 1-26 values from alphabet
-  let mut array: [Vec<i32>; st] = [vect; st];  
+  let mut array: [Vec<u32>; st] = [vect; st];  
   println!("crates: {}", stcks);
   
   let col_lngth: usize = (stcks*3 + stcks-1).into();
   println!("length of row: {}", col_lngth);
 
-  let mut lines = strt.lines();
+  let mut lines = strt.lines().enumerate();
 
   let crt_typs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  for i in 0..col_lngth {
-    while let Some(line) = lines.next() {
-        let line_bytes = line.as_bytes();
-        for i in 0..line_bytes.len() {
-          
-          println!("{} and its line {}", line_bytes[i] as char, (((i as f32)/4.0)).ceil());
-        } 
-  
+  while let Some((j,line)) = lines.next() {
+    for i in 0..col_lngth {  
+
+      let line_bytes = line.as_bytes();
+      for i in 0..line_bytes.len() {
+        let crt_ln = ((i as f32)/4.0).ceil() as usize;
+        let crt = line_bytes[i] as char;
+
+        match crt_typs.find(crt) {
+         Some(status) => {
+            println!("crate {} is going into stack {}", crt, crt_ln);
+            array[crt_ln-1].push(status as u32);
+          },
+          None => println!("not a crate"),
+        }
+         // println!("{} and its line {}", crt, crt_ln);
+      } 
+ 
 
  
-      //let crt = line[0..3].chars().next().expect("string empty") as i32;
-      //array[i].push(crt);
-      //println!("Got line: {}", crt);
     }
   }
 }
