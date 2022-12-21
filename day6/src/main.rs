e6b0e53881e032affd65f2ff84dae14790727b78
Brian Machado <bmachado@live.ca>
2022-12-20 20:15:53 -0800

    day6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -6,25 +6,28 @@ fn main() {
     
     const npt: &str = include_str!("input.txt");
     
-    let mut arr_prev = ['a', 'a', 'a', 'a'];
-    let mut arr_cur = ['a','a','a','a'];
+    let mut arr_prev = ['a';14];
+    let mut arr_cur = ['a';14];
     //populate first 4
 
     let mut tmp = npt.chars().enumerate();
     
-    for i in 0..4 {
+    for i in 0..14 {
       arr_prev[i] = tmp.next().unwrap().1;
     }
     
-    for i in 0..4 {
+    for i in 0..14 {
       arr_cur[i] = tmp.next().unwrap().1;
     }
 
     let mut found = true;
+    
+    
+
     while let Some((l, c)) = tmp.next() {
       found = true;
-      for i in 0..4 {
-        for j in 0..4 {
+      for i in 0..14 {
+        for j in 0..14 {
           if arr_prev[i] == arr_cur[j]{
             found = false;
           }
@@ -42,16 +45,13 @@ fn main() {
         println!("starts at {}", l);
         break;
       }
+    
+      arr_prev.rotate_left(1);
 
-      arr_prev[0] = arr_prev[1];
-      arr_prev[1] = arr_prev[2]; 
-      arr_prev[2] = arr_prev[3];
-      arr_prev[3] = arr_cur[0];
+      arr_prev[13] = arr_cur[0];
       
-      arr_cur[0] = arr_cur[1];
-      arr_cur[1] = arr_cur[2];
-      arr_cur[2] = arr_cur[3];
-      arr_cur[3] = c;
+      arr_cur.rotate_left(1); 
+      arr_cur[13] = c;
       
     }
 }
