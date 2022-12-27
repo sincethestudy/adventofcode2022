ea050f44e3c24af7d13b1abfdbf887b908fee31f
Brian Machado <bmachado@live.ca>
2022-12-26 22:34:38 -0500

    working tree structure

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,70 +1,46 @@
+use std::rc::{Rc, Weak};
 use std::cell::RefCell;
-use std::rc::Rc;
-use std::collections::{HashSet, HashMap};
 
-
-fn main() {
-  const input: &str = include_str!("input.txt");
-  let mut lines = input.lines();
-
-  let mut root = Node {
-    size: 0,
-    children: HashMap::new()
-  };
-
-  let mut currentNode = &mut root;
-
-
-  //add files in the current directory
-  for line in lines {
-    let mut firstIter = line.split(" ");
-    let first = firstIter.next().unwrap();
-    let second = firstIter.next().unwrap();
-    
-    match firstIter.next() {
-      Some(third) => {
-        // check for command
-        if line.chars().nth(2).unwrap() == 'c' {
-          println!("change directory: {} {} {}",first, second, third);
-
-          if third == ".." {
-            println!("implement up a dir");
-          }
-          else {
-            currentNode.add_child(third.to_string(), 0);
-            currentNode = currentNode.children.get_mut(third).unwrap();
-          }
-        }
-      },
-      None => //do nothinig
-        ()
-    }
-
-
-    if line.chars().nth(0).unwrap().is_numeric() {
-      currentNode.size += first.parse::<usize>().unwrap();
-    }
-
-  }
-  println!("{:?}", root);
-
-}
-
-#[derive(Debug)]
 struct Node {
-  size: usize,
-  children: HashMap<String, Node>
+  value: i32,
+  children: RefCell<Vec<Rc<Node>>>,
+  parent: RefCell<Weak<Node>>
 }
 
 impl Node {
-  fn new(size: usize) -> Node {
-    Node {
-      size,
-      children: HashMap::new()
-    }
+  fn add_parent(&self, node: Weak<Node>){
+    *self.parent.borrow_mut() = node;
   }
 
-  fn add_child(&mut self, name: String, size: usize) {
-    self.children.insert(name, Node::new(size));
+  fn add_child(&self, node: Rc<Node>){
+    self.children.borrow_mut().push(node)
   }
+}
+
+
+fn main() {
+
+  let leaf1 = Rc::new(Node {
+    value: 45,
+    children: RefCell::new(Vec::new()),
+    parent: RefCell::new(Weak::new())
+  });
+
+  let leaf2 = Rc::new(Node {
+    value: 55,
+    children: RefCell::new(Vec::new()),
+    parent: RefCell::new(Weak::new())
+  });
+
+  let root = Rc::new(Node {
+    value: 65,
+    children: RefCell::new(Vec::new()),
+    parent: RefCell::new(Weak::new())
+  });
+
+  leaf1.add_parent(Rc::downgrade(&root));
+
+  root.add_child(Rc::clone(&leaf2));
+
+
 }
