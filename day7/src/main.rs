d6bc6c29efd7a9c7c7d3ddb2f77b8eddeb4ddff7
Brian Machado <bmachado@live.ca>
2022-12-27 00:39:22 -0500

    working but wrong adding algoirhtnM

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,10 +1,13 @@
 use std::rc::{Rc, Weak};
 use std::cell::RefCell;
 
+#[derive(Debug, Default)]
 struct Node {
   value: RefCell<i32>,
+  name: String,
   children: RefCell<Vec<Rc<Node>>>,
-  parent: RefCell<Weak<Node>>
+  parent: RefCell<Weak<Node>>,
+  
 }
 
 impl Node {
@@ -24,27 +27,75 @@ impl Node {
 
 fn main() {
 
-  let leaf1 = Rc::new(Node {
-    value: RefCell::new(45),
-    children: RefCell::new(Vec::new()),
-    parent: RefCell::new(Weak::new())
-  });
-
-  let leaf2 = Rc::new(Node {
-    value: RefCell::new(55),
-    children: RefCell::new(Vec::new()),
-    parent: RefCell::new(Weak::new())
-  });
+  let input = include_str!("input.txt");
+  let lines = input.lines();
 
   let root = Rc::new(Node {
-    value: RefCell::new(65),
+    value: RefCell::new(0),
+    name: String::from("/"),
     children: RefCell::new(Vec::new()),
     parent: RefCell::new(Weak::new())
   });
 
-  leaf1.add_parent(Rc::downgrade(&root));
+  let mut sum = 0;
 
-  root.add_child(Rc::clone(&leaf2));
+  let mut currentNode = Rc::clone(&root);
+
+  for line in lines {
+
+    if line.chars().nth(0).unwrap() == '$'{
+      if line.chars().nth(2).unwrap() == 'c' {
+
+
+        //get the third word of the line
+        let third = line.split_whitespace().nth(2).unwrap();
+
+        if third == ".."{
+          println!("going up");
+          let thisnode = Rc::clone(&currentNode);
+          currentNode = thisnode.parent.borrow().upgrade().unwrap();
+        }
+        else {
+          if *currentNode.value.borrow() <= 100000{
+            println!("adding value: {}", *currentNode.value.borrow());
+            sum += *currentNode.value.borrow();
+          }
+
+          let newNode = Rc::new(Node {
+            value: RefCell::new(0),
+            name: String::from(third),
+            children: RefCell::new(Vec::new()),
+            parent: RefCell::new(Weak::new())
+          });
+
+          newNode.add_parent(Rc::downgrade(&currentNode));
+          currentNode.add_child(Rc::clone(&newNode));
+          currentNode = newNode;
+
+        }
+
+        println!("third: {}", third);
+      }
+      else {
+
+      }
+    }
+    else {
+      if line.chars().nth(0).unwrap().is_numeric() {
+        let value = line.split_whitespace().nth(0).unwrap().parse::<i32>().unwrap();
+        currentNode.add_value(value);
+        println!("value: {}", currentNode.value.borrow());
+      }
+    }
+  }
+
+  for child in root.children.borrow().iter() {
+    println!("child: {}", child.name);
+  }
+
+  // println!("sum: {}", sum);
+
+  // println!("root: {:#?}", root);
 
 
 }
